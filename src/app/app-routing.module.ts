24f9d1e7ce6d1b1124a44da05dfdec1f840ec7ba
rakib3004 <bsse1129@iit.du.ac.bd>
2022-03-29 16:11:30 +0600

    create about us componenet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { UpdateBookComponent } from './update-book/update-book.component';
 
@@ -7,6 +8,8 @@ const routes: Routes = [
   {path:'', component:BookListComponent},
   {path:'books', component:BookListComponent},
   {path:'updateBook', component:UpdateBookComponent},
+  {path:'aboutUs', component:AboutUsComponent},
+
 ];
 
 @NgModule({
--- src/app/home-menu/home-menu.component.ts
+++ src/app/home-menu/home-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-menu',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeMenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
+  }
+
+  viewAboutUs(){
+    this.route.navigate(['aboutUs']);
   }
 
 }
