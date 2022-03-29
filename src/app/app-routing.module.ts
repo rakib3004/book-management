dd3f2156b8d147309986ada4aea80ed344e00754
rakib3004 <bsse1129@iit.du.ac.bd>
2022-03-29 14:58:52 +0600

    add routinh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookListComponent } from './book-list/book-list.component';
+import { UpdateBookComponent } from './update-book/update-book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'books', component:BookListComponent},
+  {path:'updateBook', component:UpdateBookComponent},
+  {}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
