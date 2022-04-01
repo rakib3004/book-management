8c37d1f2883022d406d18630fed4327419e7683b
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-01 09:11:52 +0600

    reformate code in about us book adding and others

--- src/app/add-book/add-book.component.ts
+++ src/app/add-book/add-book.component.ts
@@ -6,18 +6,15 @@ import { BookService } from '../book.service';
 @Component({
   selector: 'app-add-book',
   templateUrl: './add-book.component.html',
-  styleUrls: ['./add-book.component.css']
+  styleUrls: ['./add-book.component.css'],
 })
 export class AddBookComponent implements OnInit {
+  constructor(private bookService: BookService, private route: Router) {}
 
-  constructor(private bookService: BookService, private route: Router) { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   newBook = new Book();
-  addBook(newBook: Book){
-    console.log(newBook)
+  addBook(newBook: Book) {
+    console.log(newBook);
   }
-
 }
--- src/app/book.ts
+++ src/app/book.ts
@@ -3,6 +3,6 @@ export class Book
 
     id: number=0;
     name: string ="";
-    year: number=2022;
-    availability: boolean = true;
+    year: number=0;
+    availability: boolean = false;
 }
